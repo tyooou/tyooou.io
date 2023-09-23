7689512f1d63e188208d24a9f6a6d478491f5a57
Tyler Young <young.h.tyler@gmail.com>
2023-09-23 23:40:48 +1200

    fix

--- main.js
+++ main.js
@@ -2,12 +2,24 @@
 function darkMode() {
     var element = document.body;
     element.classList.toggle("darkMode");
-    document.getElementById("darkModeToggle").classList.toggle('lightModeIcon');
 
     document.getElementById("darkModeTitle").classList.toggle('darkModeText');
-    var allText = document.getElementsByTagName('button');
-        for (var i=0; i<allText.length; i++) {
-            allText[i].classList.toggle('darkModeText');
-        }
-    var allText = document.getElementsByTagName('button');
+    var allText = document.querySelectorAll('button, p');
+    for (var i=0; i<allText.length; i++) {
+        allText[i].classList.toggle('darkModeText');
+    }
+}
+
+
+
+function darkModeIconToggle() {
+    var icon = document.getElementById('darkModeIconToggle');
+
+    if (icon.getAttribute('src') == './Icons/darkmode_icon.png') {
+        icon.src = './Icons/lightmode_icon.png';
+        icon.style = 'zoom: 5%; filter: brightness(0) invert(1);';
+    } else {
+        icon.src = './Icons/darkmode_icon.png';
+        icon.style = 'zoom: 5%; filter: brightness(0.3)';
+    }
 }
