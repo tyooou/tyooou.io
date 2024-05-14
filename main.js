57c034190ebd68717a99f2179b1c56f06b74b05a
Tyler <young.h.tyler@gmail.com>
2024-05-14 17:23:49 +1200

    Major commit.

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-
 function darkMode() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-    document.getElementById("darkModeToggle").classList.toggle('lightModeIcon');
+  document.body.classList.toggle("darkMode");
+  let allText = document.querySelectorAll("button, p, h1");
+  let icon = document.getElementById("darkModeIconToggle");
+  for (var i = 0; i < allText.length; i++) {
+    allText[i].classList.toggle("darkModeText");
+  }
 
-    document.getElementById("darkModeTitle").classList.toggle('darkModeText');
-    var allText = document.getElementsByTagName('button');
-        for (var i=0; i<allText.length; i++) {
-            allText[i].classList.toggle('darkModeText');
-        }
-    var allText = document.getElementsByTagName('button');
+  allIcons = document.querySelectorAll(".darkModeIcon, .btn-icon, .btn-down");
+  for (let j = 0; j < allIcons.length; j++) {
+    allIcons[j].style =
+      icon.getAttribute("src") == "./Icons/darkmode_icon.png"
+        ? "filter: brightness(0) invert(1);"
+        : "filter: brightness(0.3)";
+  }
+
+  icon.src =
+    icon.getAttribute("src") == "./Icons/darkmode_icon.png"
+      ? "./Icons/lightmode_icon.png"
+      : "./Icons/darkmode_icon.png";
 }
+
+function projectBtn() {
+  document.getElementById("dropdownId").classList.toggle("show");
+}
+
+window.onload = darkMode;
+
+window.transitionToPage = function (href) {
+  document.querySelector("body").style.opacity = 0;
+  setTimeout(function () {
+    window.location.href = href;
+  }, 500);
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector("body").style.opacity = 1;
+});
